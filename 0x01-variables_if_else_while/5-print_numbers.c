66d4348c877edd583dfa57ec3a4e141cd4087c9e
Miracle Ohuka <miralexohuka12@gmail.com>
2023-02-17 19:35:47 +0000

    code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point for program
+ * Return: 0 success
+ */
+int main(void)
+{
+	int a;
+
+	for(a = '0'; a <= '9'; a++)
+		putchar(a);
+	putchar('\n');
+	return(0);
+}
