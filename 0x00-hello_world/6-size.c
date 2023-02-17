7adf236e0b6c67d078c0a9e002ef6853e9acecd7
Miracle Ohuka <miralexohuka12@gmail.com>
2023-02-17 00:05:29 +0000

    last part

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("size of a char: %ld byte(s)\n", sizeof(a));
-	printf("size of a int: %ld byte(s)\n", sizeof(b));
-	printf("size of a long int: %ld byte(s)\n", sizeof(c));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(d));
-	printf("size of a float: %ld byte(s)\n", sizeof(e));
+	printf("size of a char: %d byte(s)\n", sizeof(a));
+	printf("size of a int: %d byte(s)\n", sizeof(b));
+	printf("size of a long int: %d byte(s)\n", sizeof(c));
+	printf("size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("size of a float: %d byte(s)\n", sizeof(e));
 }
