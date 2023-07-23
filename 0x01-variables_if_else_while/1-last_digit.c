5bd28b2af9846cb860aa618546eed3e0d9ca61d0
miracle ohuka <miralexohuka12@gmail.com>
2023-07-23 20:18:19 +0000

    task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ int main(void)
 	}
 	else if (x == 0)
 	{
-	printf("Last digit of %d is %d and is zero\n", n, x);
+	printf("Last digit of %d is %d and is 0\n", n, x);
 	}
 	else if (x < 6 && x != 0)
 	{
-	printf("Last digit of %d is %d and is less than 6 and not zero\n", n, x);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	}
 	return (0);
 }
